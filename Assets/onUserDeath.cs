da332f22bae2b9ed808b85438dd4054ed963257b
Chris Yoon <Chris Yoon>
2017-03-18 22:42:29 -0700

    Added walls

---
+++ Assets/onUserDeath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onUserDeath : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+		print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+		print("Their relative velocity is " + collisionInfo.relativeVelocity);
+	}
+
+}
